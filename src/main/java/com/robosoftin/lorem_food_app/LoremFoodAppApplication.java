9003aa34012f59cace3c6206f6736af2c670d48e
Ankitha <ankitha@robosoftin.com>
2023-05-03 14:55:08 +0530

    Initial commit

---
+++ src/main/java/com/robosoftin/lorem_food_app/LoremFoodAppApplication.java
@@ -0,0 +1,13 @@
+package com.robosoftin.lorem_food_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoremFoodAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoremFoodAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robosoftin/lorem_food_app/LoremFoodAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robosoftin.lorem_food_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoremFoodAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
