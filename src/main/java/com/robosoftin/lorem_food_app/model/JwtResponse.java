0a68518f34f49c0a0943f15dca0ed9bd67fdb7d0
Ankitha <ankitha@robosoftin.com>
2023-05-07 14:59:53 +0530

    commit

--- src/main/java/com/robosoftin/lorem_food_app/model/JwtResponse.java
+++ src/main/java/com/robosoftin/lorem_food_app/model/JwtResponse.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,4 +13,5 @@ public class JwtResponse {
     private String jwtToken;
     private String refreshToken;
     private String message;
+    private Map<String,String> userDetails;
 }
